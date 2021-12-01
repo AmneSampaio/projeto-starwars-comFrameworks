1d376aed50906def7c887010c0439b3247c3c627
AmneSampaio <amnesf@gmail.com>
2021-12-01 16:34:09 -0300

    Adds configuration Bean for RestTemplate

--- src/main/java/com/cinema/starwars/StarwarsApplication.java
+++ src/main/java/com/cinema/starwars/StarwarsApplication.java
@@ -2,9 +2,17 @@ package com.cinema.starwars;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@Configuration
 public class StarwarsApplication {
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(StarwarsApplication.class, args);
