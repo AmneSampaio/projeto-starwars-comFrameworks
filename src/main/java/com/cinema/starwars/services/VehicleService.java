cbeafb546e60ce221535788493d6f00f403f7447
AmneSampaio <amnesf@gmail.com>
2021-12-01 16:43:45 -0300

    Adds a url to return one vehicle

--- src/main/java/com/cinema/starwars/services/VehicleService.java
+++ src/main/java/com/cinema/starwars/services/VehicleService.java
@@ -2,6 +2,7 @@ package com.cinema.starwars.services;
 
 import com.cinema.starwars.controllers.VehicleController;
 import com.cinema.starwars.models.Garage;
+import com.cinema.starwars.models.Vehicle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -15,9 +16,17 @@ public class VehicleService {
     @Autowired
     private RestTemplate restTemplate;
 
-    public Garage getGarage(){
+    public Garage getEntireGarage(){
         return restTemplate.getForObject("https://swapi.dev/api/vehicles", Garage.class);
     }
+
+    public Vehicle getOneVehicleFromGarage(int id){
+        return restTemplate.getForObject("https://swapi.dev/api/vehicles/"+id, Vehicle.class);
+    }
+
+
+
+
 
 
 }
