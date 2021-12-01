fd560cd7bcd9540d103ebb59560d9500ea5186b3
AmneSampaio <amnesf@gmail.com>
2021-12-01 16:28:32 -0300

    Creating class Vehicle

---
+++ src/main/java/com/cinema/starwars/models/Vehicle.java
@@ -0,0 +1,55 @@
+package com.cinema.starwars.models;
+
+public class Vehicle {
+
+    private Long id;
+    private String name;
+    private String model;
+
+    public Vehicle() {
+    }
+
+    public Vehicle(String name, String model) {
+        this.name = name;
+        this.model = model;
+    }
+
+    public Vehicle(Long id, String name, String model) {
+        this.id = id;
+        this.name = name;
+        this.model = model;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehicle{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", model='" + model + '\'' +
+                '}';
+    }
+}
