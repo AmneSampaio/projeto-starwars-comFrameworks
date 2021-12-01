538da9ed02d42082134384524ff794c7a1709ebb
AmneSampaio <amnesf@gmail.com>
2021-12-01 16:33:20 -0300

    Creating initial controller, service and repository

---
+++ src/main/java/com/cinema/starwars/controllers/VehicleController.java
@@ -0,0 +1,21 @@
+package com.cinema.starwars.controllers;
+
+import com.cinema.starwars.models.Garage;
+import com.cinema.starwars.services.VehicleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/starwars")
+public class VehicleController {
+
+    @Autowired
+    private VehicleService vehicleService;
+
+    @GetMapping("all")
+    public Garage getGarage(){
+        return vehicleService.getGarage();
+    }
+}
