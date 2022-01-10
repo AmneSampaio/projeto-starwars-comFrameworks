0a689ee9cb475216525048a6c71dd8a3ee7d439b
AmneSampaio <amnesf@gmail.com>
2022-01-10 13:36:40 -0300

    Adding attribute cost_in_credits and 'auctions' classes.

--- src/main/java/com/cinema/starwars/models/Vehicle.java
+++ src/main/java/com/cinema/starwars/models/Vehicle.java
@@ -1,32 +1,42 @@
 package com.cinema.starwars.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Vehicle {
 
+    @Id
+    @GeneratedValue
+    private Long id;
     private String name;
     private String model;
+    private String cost_in_credits;
 
     public Vehicle() {
     }
 
-    public Vehicle(String name, String model) {
+    public Vehicle(String name, String model, String cost_in_credits) {
         this.name = name;
         this.model = model;
+        this.cost_in_credits = cost_in_credits;
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getModel() {
         return model;
     }
 
-    public void setModel(String model) {
-        this.model = model;
+    public String getCost_in_credits() {
+        return this.cost_in_credits;
     }
 
     @Override
@@ -34,6 +44,7 @@ public class Vehicle {
         return "Vehicle{" +
                 ", name='" + name + '\'' +
                 ", model='" + model + '\'' +
+                ", cost_in_credits=" + cost_in_credits +
                 '}';
     }
 }
